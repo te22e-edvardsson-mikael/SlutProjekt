97bd4e107dee012322ab8ee34312dd9a986163e5
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-03-12 11:11:04 +0100

    tog min gamla kod och fixade up den/bugs

--- slutProjekt/Program.cs
+++ slutProjekt/Program.cs
@@ -1,27 +1,209 @@
-﻿using Raylib_cs;
+﻿
+using System.Diagnostics.Contracts;
+using Raylib_cs;
 using System.Numerics;
 
-Raylib.InitWindow(800, 600, "joebiden");
+
+Raylib.InitWindow(1800, 900, "vinterprojekt");
 Raylib.SetTargetFPS(60);
+
+
+//variabler//
+int enemyColorss = 0;
+
+string scene;
+string scene2;
+
+scene = "start";
+
+scene2 = "victory";
+
+int liv = 1;
+
+int score = 0;
+
+float enemyVelocityY = 1f;
+
+bool spaceKeyReleased = true;
+
+
+
+//listor//
 
 List<Rectangle> walls = new();
 
-walls.Add(new Rectangle(0,800,20,20));
-walls.Add(new Rectangle(600,800,20,20));
-walls.Add(new Rectangle(600,0,20,20));
-walls.Add(new Rectangle(600,400,20,20));
+
+walls.Add(new Rectangle(300, 100, 60, 20));
+walls.Add(new Rectangle(320, 0, 16, 200));
+walls.Add(new Rectangle(300, 0, 32, 128));
+walls.Add(new Rectangle(300, 600, 100, 128));
+walls.Add(new Rectangle(1000, 0, 50, 128));
+walls.Add(new Rectangle(1000, 600, 50, 128));
+walls.Add(new Rectangle(800, 100, 50, 128));
+walls.Add(new Rectangle(1600, 70, 50, 128));
+walls.Add(new Rectangle(1400, 300, 50, 128));
+
+
+walls.Add(new Rectangle(0, 0, 1800, 20));
+walls.Add(new Rectangle(0, 880, 1800, 20));
+walls.Add(new Rectangle(1780, 0, 20, 900));
+walls.Add(new Rectangle(0, 0, 20, 900));
 
 
 
-while (!Raylib.WindowShouldClose()){
+Rectangle rRect = new Rectangle(275, 260, 250, 100);
+Rectangle playerRect = new Rectangle(1, 50, 100, 100);
+Rectangle enemyRect = new Rectangle(1000, 100, 100, 100);
+
+
+
+Color[] enemyColors = new Color[] { Color.Blue, Color.Purple, Color.Red, Color.Orange };
+
+
+/*--------------------------------------------------------------------------------------------------------------*/
+
+while (!Raylib.WindowShouldClose())
+{
+
+  if (scene == "start")
+  {
+
+    Raylib.ClearBackground(Color.Black);
+    Raylib.DrawRectangleRec(rRect, Color.Black);
+    Raylib.DrawText("Press space to start", 290, 300, 20, Color.Red);
+
+    if (Raylib.IsKeyPressed(KeyboardKey.Space) && spaceKeyReleased)
+    {
+      Raylib.ClearBackground(Color.Black);
+
+      scene = "game";
+
+      playerRect.X = 21;
+      playerRect.Y = 50;
+
+      spaceKeyReleased = false;
+    }
+
+    if (Raylib.IsKeyReleased(KeyboardKey.Space))
+    {
+      spaceKeyReleased = true;
+    }
+  }
+
+  else if (scene == "game")
+  
     Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+{
+    //render
+    Raylib.DrawText($"points {score}", 50, 520, 40, Color.Gray);
+    Raylib.DrawText($"Health {liv}", 250, 520, 40, Color.Gray);
+    Raylib.DrawRectangleRec(playerRect, Color.Red);
+    Raylib.DrawRectangleRec(enemyRect, enemyColors[enemyColorss]);
 
-    
- 
+
+    //-------------------------------------------------------------------------------
+
+    //logic
+    enemyRect.Y += enemyVelocityY;
+
+    if (enemyRect.Y <= 0 || enemyRect.Y + 100 >= 900)
+    {
+      enemyVelocityY = -enemyVelocityY;
+    }
+
+
+
+    foreach (Rectangle wall in walls)
+    {
+      Raylib.DrawRectangleRec(wall, Color.DarkBlue);
+    }
+
+
+
+
+    //rörelse för player
+
+    if (Raylib.IsKeyDown(KeyboardKey.D))
+    {
+      playerRect.X += 5;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.A))
+    {
+      playerRect.X -= 5;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.W))
+    {
+      playerRect.Y -= 5;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.S))
+    {
+      playerRect.Y += 5;
+    }
+
+
+    //-------------------------------------------------------------------------
+
+    //räkna ints
+
+    bool isInAWall = false;
+
+    for (int i = 0; i < walls.Count; i++)
+    {
+
+      if (Raylib.CheckCollisionRecs(playerRect, walls[i]))
+      {
+        isInAWall = true;
+      }
+    }
+
+    if (isInAWall == true)
+    {
+
+      liv--;
+
+      if (liv < 0)
+      {
+        scene = "start";
+        liv = 1;
+        spaceKeyReleased = true;
+      }
+
+    }
 
 
 
 
 
-    Raylib.EndDrawing();
+    //---------------------------------------------------------------------
+
+    //funktioner för enemyrect
+
+    if (Raylib.CheckCollisionRecs(playerRect, enemyRect))
+    {
+
+      enemyColorss = (enemyColorss + 1) % enemyColors.Length;
+
+      score++;
+
+      Random random = new Random();
+      enemyRect.X = random.Next(0, 1800);
+      enemyRect.Y = random.Next(0, 800);
+    }
+
+  }
+  if (score == 15)
+  {
+    scene2 = "victory";
+
+
+    if (scene2 == "victory")
+    {
+      Raylib.ClearBackground(Color.Black);
+      Raylib.DrawText("victory", 900, 450, 10, Color.Violet);
+    }
+  }
+
+  Raylib.EndDrawing();
 }
