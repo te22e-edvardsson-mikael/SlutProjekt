4f4654d4e381759df1f2f0aa4a62c663569069b4
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-03-12 10:28:22 +0100

    lista av rectanglar

--- slutProjekt/Program.cs
+++ slutProjekt/Program.cs
@@ -1,12 +1,23 @@
 ﻿using Raylib_cs;
-
+using System.Numerics;
 
 Raylib.InitWindow(800, 600, "joebiden");
 Raylib.SetTargetFPS(60);
 
+List<Rectangle> walls = new();
+
+walls.Add(new Rectangle(0,800,20,20));
+walls.Add(new Rectangle(600,800,20,20));
+walls.Add(new Rectangle(600,0,20,20));
+walls.Add(new Rectangle(600,400,20,20));
+
+
+
 while (!Raylib.WindowShouldClose()){
     Raylib.BeginDrawing();
 
+    
+ 
 
 
 
